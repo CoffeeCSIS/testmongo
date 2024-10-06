749119323dc3668e1cd46928913b1c4235446553
CoffeeCSIS <coffee.csis2020@gmail.com>
2024-10-06 16:59:51 +0800

    Update testapi.js

--- api/testapi.js
+++ api/testapi.js
@@ -1,12 +1,5 @@
-export default async function handler(req, res) {
-  const city = req.query.city || 'New York';
-  const apiKey = 'YOUR_WEATHER_API_KEY';
-  //const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
-  //const data = await response.json();
+// /api/testapi.js
 
-  res.status(200).json({
-    city: "Malaysia",  // data.location.name,
-    temperature: "37.5",  // data.current.temp_c,
-    condition: "Cloudy",   //data.current.condition.text,
-  });
-}
+module.exports = (req, res) => {
+  res.status(200).json({ message: "Hello from Vercel API!" });
+};
